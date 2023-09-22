40495108424fe6b72475a056ac36a660789263ae
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-22 11:39:39 +1000

    initial setup of router

--- app.js
+++ app.js
@@ -9,15 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  console.log("GET");
-  res.send({ firstName: Bart, lastName: Delbecke });
-});
+const userRouter = require("./routes/users");
 
-app.post("/", (req, res) => {
-  console.log("POST");
-  console.log("req.body");
-});
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
