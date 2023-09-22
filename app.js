e65061610becb6d53c90517560a58fc2b55ce7b5
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-23 08:44:58 +1000

    users router

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ const userRouter = require("./routes/users");
 app.use("/users", userRouter);
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+	console.log(`server is running on port ${PORT}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,23 @@ const router = express.Router();
 const { sequelize, User } = require("../models");
 
 router.get("/", (req, res) => {
-  console.log("#### GET All Users ####");
-  res.json({ message: "GET ALL Users" });
+	console.log("#### GET All Users ####");
+
+	res.json({ message: "GET ALL Users" });
 });
 
 router.post("/new", async (req, res) => {
-  console.log("#### POST New User ####");
+	console.log("#### POST New User #######");
 
-  const { firstName, lastName } = req.body;
+	const { firstName, lastName } = req.body;
 
-  try {
-    const user = await User.create({ firstName, lastName });
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.res.status(500).json({ error: "Something went wrong" });
-  }
+	try {
+		const user = await User.create({ firstName, lastName });
+		res.json(user);
+	} catch (error) {
+		console.error(error);
+		res.res.status(500).json({ error: "Something went wrong" });
+	}
 });
 
 module.exports = router;
