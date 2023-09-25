d67c7d947a89790c46751fcdcaa83ef390076e11
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-25 15:50:28 +1000

    users router: sign in and sign up

--- routes/users.js
+++ routes/users.js
@@ -10,21 +10,51 @@ router.get("/", (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-	console.log("#### POST New User SignUp #######");
-
 	const { firstName, lastName, email, password } = req.body;
 
-	console.log("firstName", firstName);
-	console.log("lastName", lastName);
-	console.log("email", email);
-	console.log("password", password);
-
 	try {
-		const user = await User.create({ firstName, lastName, email, password });
-		res.json(user);
+		const check = await User.findOne({
+			where: {
+				email: email,
+			},
+		});
+		if (!check) {
+			const user = await User.create({ firstName, lastName, email, password });
+			res.json(user);
+		} else {
+			res.json({ message: "email already in use" });
+		}
 	} catch (error) {
 		console.error(error);
 		res.res.status(500).json({ error: "Something went wrong" });
+	}
+});
+
+router.post("/signin", async (req, res) => {
+	console.log("############## SIGN IN ################");
+	const { email, password } = req.body;
+
+	console.log("Email", email);
+	console.log("Password", password);
+
+	try {
+		const user = await User.findOne({
+			where: {
+				email: email,
+			},
+		});
+		console.log("USER:", user);
+		if (user) {
+			if (password === user.dataValues.password) {
+				res.json({ ...user.dataValues, correct: true });
+			} else {
+				res.json({ message: "incorrect password" });
+			}
+		} else {
+			res.json({ message: "user doesn't exist" });
+		}
+	} catch (error) {
+		console.error(error);
 	}
 });
 
